17172504935e27f5c71275d09592cfab5b9350bb
scdRobotics <logandavid555@gmail.com>
2021-04-16 17:04:48 -0400

    Velocity Write To File Done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WriteToFile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WriteToFile.java
@@ -3,16 +3,17 @@ package org.firstinspires.ftc.teamcode;
 import android.os.Environment;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 
-        /**
-         * Created by Gabriel on 2018-01-03.
-         * A simple way to log data to a file.
-         */
+/**
+ * Created by Gabriel on 2018-01-03.
+ * A simple way to log data to a file.
+ */
 @Autonomous(name="WriteFileTest", group="Linear Opmode")
 public class WriteToFile extends AutonomousPrime2020{
 
@@ -22,6 +23,12 @@ public class WriteToFile extends AutonomousPrime2020{
     private boolean logTime;
     private long startTime;
     private boolean disabled = false;
+
+
+    ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    /*
+    Probably will throw an error- see what stuff Android Studio suggests to make it
+    */
 
     public void Log(String filename, boolean logTime) {
         if (logTime) startTime = System.nanoTime();
@@ -68,8 +75,12 @@ public class WriteToFile extends AutonomousPrime2020{
     }
 
     public void addData(String data) {
-        if (disabled) return;
-        if (!line.equals("")) line += ",";
+        if (disabled){
+            return;
+        }
+        if (!line.equals("")) {
+            line += ",";
+        }
         line += data;
     }
     public void addData(Object data) {
@@ -103,14 +114,31 @@ public class WriteToFile extends AutonomousPrime2020{
     public void runOpMode(){
         mapObjects();
         waitForStart();
-        if (opModeIsActive()) {
-            Log("test", false);
-            addData(5);
-            addData("Test");
-            addData("\n");
-            addData("a");
-            update();
-            close();
+        Log("FinalVelocityOverTime", false); //Make file
+        addData("LaunchRight Velocity");
+        addData("LaunchLeft Velocity");
+        addData("Elapsed Time");
+        update(); //Update File
+        velocitySpin(1, 1020); //Spin wheels
+        System.out.println("Start Loop");
+        timer.reset(); //Reset timer
+        boolean loop = true;
+        while (opModeIsActive() && loop) { //Loop
+            addData(launchRight.getVelocity()); //Record launch right motor velocity
+            System.out.println("In Loop");
+            telemetry.addData("Y'ello?", "Who dis?");
+            telemetry.update();
+            addData(launchLeft.getVelocity()); //Record launch left wheel motor velocity
+            addData(timer.time()); //Add timer when recording happens
+            //addData("\n"); //New line
+            update(); //Update File- might want to update file when is stop requested to save time?? Risky if function fails to complete before stop, though
+            if(timer.time()>=300000){
+                update();
+                System.out.println("Loop End");
+                telemetry.update();
+                close();
+                loop=false;
+            }
         }
     }
 
